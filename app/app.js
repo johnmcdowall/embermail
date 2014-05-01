4c939a0d87c91f7910b3dcab1646a8732eec8df4
Erik Bryn <erik.bryn@gmail.com>
2014-05-01 09:17:55 -0700

    Reset things

--- app/app.js
+++ app/app.js
@@ -1,8 +1,5 @@
-/* globals Pretender */
-
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
-import RouteRecognizer from "route-recognizer";
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
@@ -15,115 +12,115 @@ loadInitializers(App, 'embermail');
 
 // START FAKE API SERVER DEFINITION
 
-function jsonify(obj) {
-  return JSON.stringify(obj);
-}
+// function jsonify(obj) {
+//   return JSON.stringify(obj);
+// }
 
-function response(obj) {
-  var responseCode = obj ? 200 : 404;
-  return [responseCode, {'Content-Type': 'application/json'}, obj ? jsonify(obj) : ""];
-}
+// function response(obj) {
+//   var responseCode = obj ? 200 : 404;
+//   return [responseCode, {'Content-Type': 'application/json'}, obj ? jsonify(obj) : ""];
+// }
 
-window.RouteRecognizer = RouteRecognizer; // Hack for Pretender
+// window.RouteRecognizer = requireModule('route-recognizer')['default']; // Hack for Pretender
 
-new Pretender(function(){
-  this.get('/v1/messages', function(){
-    var messages = [];
-    for (var i = 1, l = 10; i <= l; i++) {
-      messages.push(generateMessage("Message #" + i, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + i));
-    }
-    return response(messages);
-  });
+// new Pretender(function(){
+//   this.get('/v1/messages', function(){
+//     var messages = [];
+//     for (var i = 1, l = 10; i <= l; i++) {
+//       messages.push(generateMessage("Message #" + i, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + i));
+//     }
+//     return response(messages);
+//   });
 
-  this.get('/v1/messages/:message_id', function(req) {
-    var id = parseInt(req.params.message_id, 10) + 1;
+//   this.get('/v1/messages/:message_id', function(req) {
+//     var id = parseInt(req.params.message_id, 10) + 1;
 
-    return response(generateMessage("Message #" + id, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + id));
-  });
+//     return response(generateMessage("Message #" + id, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + id));
+//   });
 
-  this.get('/v2/conversations', function() {
-    var conversations = [];
-    for (var i = 1, l = 10; i <= l; i++) {
-      conversations.push({
-        id: conversationGuid++,
-        messages: [
-          generateMessage("Message #" + i, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + i)
-        ]
-      });
-    }
-    return response(conversations);
-  });
+//   this.get('/v2/conversations', function() {
+//     var conversations = [];
+//     for (var i = 1, l = 10; i <= l; i++) {
+//       conversations.push({
+//         id: conversationGuid++,
+//         messages: [
+//           generateMessage("Message #" + i, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + i)
+//         ]
+//       });
+//     }
+//     return response(conversations);
+//   });
 
-  this.get('/v2/conversations/:conversation_id', function(req) {
-    var id = parseInt(req.params.conversation_id, 10);
+//   this.get('/v2/conversations/:conversation_id', function(req) {
+//     var id = parseInt(req.params.conversation_id, 10);
 
-    return response({
-      id: id,
-      messages: [
-        generateMessage("Message #" + id, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + id)
-      ]
-    });
-  });
+//     return response({
+//       id: id,
+//       messages: [
+//         generateMessage("Message #" + id, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + id)
+//       ]
+//     });
+//   });
 
 
-  this.get('/v3/folders', function() {
-    var conversations = [];
-    for (var i = 1, l = 10; i <= l; i++) {
-      conversations.push({
-        id: conversationGuid++,
-        messages: [
-          generateMessage("Message #" + i, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + i)
-        ]
-      });
-    }
-    return response([{
-      id: 'inbox',
-      name: 'Inbox',
-      conversations: conversations
-    }, {
-      id: 'spam',
-      name: 'Spam',
-      conversations: [
-        {
-          id: conversationGuid++,
-          messages: [generateMessage("This is spam", "some@jerk.com", ["you@example.com"], "Spam spam spam spam")]
-        }
-      ]
-    }]);
-  });
+//   this.get('/v3/folders', function() {
+//     var conversations = [];
+//     for (var i = 1, l = 10; i <= l; i++) {
+//       conversations.push({
+//         id: conversationGuid++,
+//         messages: [
+//           generateMessage("Message #" + i, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + i)
+//         ]
+//       });
+//     }
+//     return response([{
+//       id: 'inbox',
+//       name: 'Inbox',
+//       conversations: conversations
+//     }, {
+//       id: 'spam',
+//       name: 'Spam',
+//       conversations: [
+//         {
+//           id: conversationGuid++,
+//           messages: [generateMessage("This is spam", "some@jerk.com", ["you@example.com"], "Spam spam spam spam")]
+//         }
+//       ]
+//     }]);
+//   });
 
-  this.get('/v3/folders/:folder_id', function(req) {
-    var id = req.params.folder_id;
+//   this.get('/v3/folders/:folder_id', function(req) {
+//     var id = req.params.folder_id;
 
-    var conversations = [];
-    for (var i = 1, l = 10; i <= l; i++) {
-      conversations.push({
-        id: conversationGuid++,
-        messages: [
-          generateMessage("Message #" + i, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + i)
-        ]
-      });
-    }
+//     var conversations = [];
+//     for (var i = 1, l = 10; i <= l; i++) {
+//       conversations.push({
+//         id: conversationGuid++,
+//         messages: [
+//           generateMessage("Message #" + i, "foo@bar.com", ["you@example.com"], "Hello, this is message #" + i)
+//         ]
+//       });
+//     }
 
-    return response({
-      id: id,
-      name: id, // FIXME
-      conversations: conversations
-    });
-  });
-});
+//     return response({
+//       id: id,
+//       name: id, // FIXME
+//       conversations: conversations
+//     });
+//   });
+// });
 
-var conversationGuid = 1;
-var messageGuid = 1;
-function generateMessage(subject, from, to, body) {
-  return {
-    id: messageGuid++,
-    subject: subject,
-    from: from,
-    to: to,
-    time: new Date(),
-    body: body
-  };
-}
+// var conversationGuid = 1;
+// var messageGuid = 1;
+// function generateMessage(subject, from, to, body) {
+//   return {
+//     id: messageGuid++,
+//     subject: subject,
+//     from: from,
+//     to: to,
+//     time: new Date(),
+//     body: body
+//   };
+// }
 
 export default App;
--- app/router.js
+++ app/router.js
@@ -3,16 +3,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  // this.route('message', {path: 'message/:message_id'});
 
-  // this.route('conversation', {path: 'conversation/:conversation_id'});
-
-  // this.resource('folders', function() {
-  //   this.resource('folder', {path: ":folder_id"}, function() {
-  //     this.resource('conversations', {path: '/'});
-  //     this.resource('conversation', {path: 'conversation/:conversation_id'});
-  //   });
-  // });
 });
 
 export default Router;
